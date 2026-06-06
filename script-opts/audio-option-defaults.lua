29dff07c40e6fde2d243bba22c0aad9ce6f9dff6
Ckrvxr <85326108+Ckrvxr@users.noreply.github.com>
2026-06-06 15:28:35 +0800

    fix: resolve merge conflict - use local sofa config with sofas/ path

--- script-opts/audio-option-defaults.lua
+++ script-opts/audio-option-defaults.lua
@@ -1,5 +1,10 @@
 return {
-    sofalizer = "lavfi=[sofalizer=sofa='C\\:/Users/Ckrvxr/scoop/apps/mpv/current/portable_config/models/SADIEII_D1_48K_24bit_256tap_FIR_SOFA.sofa']",
-    loudnorm = "lavfi=[loudnorm=I=-16.0:TP=-1.0:LRA=12.0]",
-    loudnorm_id = "Apple_Music",
+    sofa_preset = "SADIEII_D1_48K_24bit_256tap_FIR_SOFA",
+    sofa_radius = 1.00,
+    sofa_gain = 12,
+    sofalizer = "lavfi=[sofalizer=sofa='C\\:/Users/Ckrvxr/scoop/apps/mpv/current/portable_config/models/sofas/SADIEII_D1_48K_24bit_256tap_FIR_SOFA.sofa':radius=1.00:gain=12]",
+    loudnorm = '',
+    loudnorm_id = "BYPASS",
+    eq = '',
+    eq_file = nil,
 }
